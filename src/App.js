523def2ddaa518b233a4697e365aadc65e4850a5
pcapinc <alex@pulsecapture.inc>
2025-03-03 19:43:02 -0800

    Updated features

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ScrollIndicator from "./components/ScrollIndicator";
 
 
 
+
 function App() {
   const [scroll, setScroll] = useState(0);
   const [isScrolling, setIsScrolling] = useState(false);
